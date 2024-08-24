a838389432e107abf3b51b6666353fd94e8ac667
gomes-tkd <jgomestkd@gmail.com>
2024-08-24 20:39:15 -0300

    Adding FIREBASE

---
+++ src/app/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAAmEWP0yY0AaPbAt9PyDG2XYqvhhCSDIA",
+    authDomain: "to-do-nextjs-jose-gomes.firebaseapp.com",
+    projectId: "to-do-nextjs-jose-gomes",
+    storageBucket: "to-do-nextjs-jose-gomes.appspot.com",
+    messagingSenderId: "108856412097",
+    appId: "1:108856412097:web:ede27f6bb8eae87f721140"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
